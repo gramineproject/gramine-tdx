b07690d65d5821c4c0bb8ec5aa5ab262ed90be08
Dmitrii Kuvaiskii <dmitrii.kuvaiskii@intel.com>
2023-05-25 02:57:02 -0700

    [PAL/VM,TDX] Account for int wrap around in virtio queues
    Previously, virtio drivers assumed that the host VMM is malicious if it
    would return a `used_idx` in some virtio queue that is less than the
    VM-memoized last seen `used_idx`. However, this case is possible on int
    wrap around. Thus, this commit modifies the check to verify that the
    host-returned index is at most queue-size items ahead of the VM-memoized
    index, accounting for int wrap.
    
    Note that this check concerns only console and vsock drivers. The fs
    driver is not problematic because it always assumes an increment of 1 in
    the virtio RX queue (this increment accounts for possible int wrap).
    
    Signed-off-by: Dmitrii Kuvaiskii <dmitrii.kuvaiskii@intel.com>

--- pal/src/host/vm-common/kernel_virtio_vsock.c
+++ pal/src/host/vm-common/kernel_virtio_vsock.c
@@ -54,8 +54,9 @@ static int handle_rq(void) {
 
     uint16_t host_used_idx = vm_shared_readw(&g_vsock->rq->used->idx);
 
-    if (host_used_idx < g_vsock->rq->seen_used) {
-        /* malicious (impossible) value reported by the host */
+    if (host_used_idx - g_vsock->rq->seen_used > g_vsock->rq->queue_size) {
+        /* malicious (impossible) value reported by the host; note that this check works also in
+         * cases of int wrap */
         return -PAL_ERROR_DENIED;
     }
 
@@ -156,8 +157,9 @@ static int cleanup_tq(void) {
 
     uint16_t host_used_idx = vm_shared_readw(&g_vsock->tq->used->idx);
 
-    if (host_used_idx < g_vsock->tq->seen_used) {
-        /* malicious (impossible) value reported by the host */
+    if (host_used_idx - g_vsock->tq->seen_used > g_vsock->tq->queue_size) {
+        /* malicious (impossible) value reported by the host; note that this check works also in
+         * cases of int wrap */
         return -PAL_ERROR_DENIED;
     }
 
