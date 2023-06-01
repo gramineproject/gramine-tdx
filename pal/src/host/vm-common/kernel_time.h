a00555b1eb07c9294c2e8c109c4933f60cbc9e6c
Dmitrii Kuvaiskii <dmitrii.kuvaiskii@intel.com>
2023-06-01 05:49:31 -0700

    [PAL/VM,TDX] Remove timeout objects via `deregister_timeout()`
    Previously, wait-with-timeout functions registered a timeout (in a
    global list) using `register_timeout()`. This func allocated a timeout
    object and added it to the list of pending timeouts. Another func
    `notify_about_timeouts_uninterruptable()` would remove the triggered
    timeout from the list and free the timeout object itself.
    
    However, if the wait-with-timeout function finishes before the timeout
    is triggered, then the timeout would become "dangling". In reality, when
    the wait-with-timeout function finishes, the timeout must be removed
    from the list and freed (i.e. by the function itself).
    
    This commit forces wait-with-timeout functions to take ownership of the
    timeout object, returned by `register_timeout()` and deregister this
    timeout upon finishing via `deregister_timeout()`. As a side effect, the
    two users of timeout -- `PalEventWait()` and `PalStreamsWaitEvents()` --
    are refactored to have uniform timeout-handling code.
    
    Signed-off-by: Dmitrii Kuvaiskii <dmitrii.kuvaiskii@intel.com>

--- pal/src/host/vm-common/kernel_time.h
+++ pal/src/host/vm-common/kernel_time.h
@@ -11,7 +11,14 @@
 #define TIME_S_STR_MAX (20 + 1)
 
 int get_time_in_us(uint64_t* out_us);
-int register_timeout(uint64_t timeout_absolute_us, int* futex);
+
+/* `timeout_out` is an opaque object to be used in `deregister_timeout()`. It is the responsibility
+ * of the caller to remove the timeout (even if the timeout was already triggered by
+ * `notify_about_timeouts_uninterruptable`). */
+int register_timeout(uint64_t timeout_absolute_us, int* futex, void** timeout_out);
+void deregister_timeout(void* timeout);
+
+int remove_timeouts_on_futex(int* futex);
 int notify_about_timeouts_uninterruptable(void);
 
 int time_init(void);
