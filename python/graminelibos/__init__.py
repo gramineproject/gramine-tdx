40b1540c33f4ca712736b0423f58466d6eac8cbf
Wenhan Ji <whji@cs.unc.edu>
2025-08-26 13:53:28 -0400

    CI
    Signed-off-by: Wenhan Ji <whji@cs.unc.edu>

--- python/graminelibos/__init__.py
+++ python/graminelibos/__init__.py
@@ -7,6 +7,8 @@ _CONFIG_PKGLIBDIR = '@PKGLIBDIR@'
 _CONFIG_LIBDIR = '@LIBDIR@'
 _CONFIG_SYSLIBDIR = '@SYSLIBDIR@'
 _CONFIG_SGX_ENABLED = '@SGX_ENABLED@' == '1'
+_CONFIG_VM_ENABLED  = '@VM_ENABLED@'  == '1'
+_CONFIG_TDX_ENABLED = '@TDX_ENABLED@' == '1'
 
 if __version__.startswith('@') and not _os.getenv('GRAMINE_IMPORT_FOR_SPHINX_ANYWAY') == '1':
     raise RuntimeError(
