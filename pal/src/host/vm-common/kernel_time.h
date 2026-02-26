89d36da3a1cac766fcbcd57df16cf4bea6a7606d
Kailun Qin <kailun.qin@intel.com>
2026-02-26 00:16:26 -0500

    [PAL/VM,TDX] Start from VMM-provided UNIX timestamp
    Previously, Gramine-VM and Gramine-TDX did not have a correct UNIX time
    on startup. They simply started with the timestamp of `0` (which in
    UNIX-time format means "1. January 1970").
    
    This commit gets the UNIX time value on startup from the VMM using
    "FW CFG" feature of QEMU. The obtained time value is untrusted so checks
    are added to guarantee it's in a reasonable range. In the future, it
    should be obtained from a trusted time source, like a trusted remote
    server.
    
    Signed-off-by: Kailun Qin <kailun.qin@intel.com>

--- pal/src/host/vm-common/kernel_time.h
+++ pal/src/host/vm-common/kernel_time.h
@@ -7,6 +7,9 @@
 
 #include <stdint.h>
 
+/* the max of Unix time in seconds is `UINT64_MAX`, which has 20 digits in decimal */
+#define TIME_S_STR_MAX (20 + 1)
+
 int get_time_in_us(uint64_t* out_us);
 int register_timeout(uint64_t timeout_absolute_us, int* futex);
 int notify_about_timeouts_uninterruptable(void);
