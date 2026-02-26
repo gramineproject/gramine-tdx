0bac0fd01b0eac6f7367853ec5be6c19cd5a0a9c
Dimitrios Stavrakakis <dimitrios.stavrakakis@intel.com>
2026-02-26 00:16:44 -0500

    [PAL/vm-common] Forward envvars from host into Gramine
    All host environment variables are collected by the `gramine-vm.in`
    template Bash script, by invoking the Python sub-script. These collected
    envvars are serialized into a single string that is passed via QEMU's
    fw_cfg pseudo-device, in selector `/opt/gramine/envs`. The serialized
    envvars are guarded by `-gramine-envs ... -gramine-envs-end` tokens.
    
    The current limits are: 16K size of the string with all collected host
    envvars, and 256 envvars in total. This seems enough on typical systems.
    
    This commit additionally escapes the commas in application name and
    host envvars, by replacing with a double-comma (QEMU formatting rule).
    This commit also renames the function/variable names, the fw_cfg
    selector and the guard tokens for cmdline arguments' parsing, making
    them uniform with envvars' parsing.
    
    Signed-off-by: Dimitrios Stavrakakis <dimitrios.stavrakakis@intel.com>

--- pal/src/host/vm-common/kernel_vmm_inputs.c
+++ pal/src/host/vm-common/kernel_vmm_inputs.c
@@ -2,8 +2,8 @@
 /* Copyright (C) 2023 Intel Corporation */
 
 /*
- * Inputs to Gramine from the host: command-line arguments, current working dir (PWD), current UNIX
- * time. Inputs are acquired via QEMU's FW CFG interface.
+ * Inputs to Gramine from the host: command-line arguments, environment variables, current working
+ * dir (PWD), current UNIX time. Inputs are acquired via QEMU's FW CFG interface.
  *
  * Notes on multi-core synchronization:
  *   - All functions are called at init, no sync required
@@ -19,49 +19,72 @@
 #include "vm_callbacks.h"
 
 char g_host_pwd[PATH_MAX];
+char g_envs[MAX_ENVS_SIZE];
 
-/* This function copies `cmdline` to a new string (we don't want to modify `cmdline`) and splits the
+/* This function copies `input` to a new string (we don't want to modify `input`) and splits the
  * new line in NUL-terminated sub-strings (arguments). Each argument may be enclosed in
  * double-quotes; in this case everything between the double-quotes (including whitespaces) is
  * considered as one argument. Having an argument without a closing double-quote leads to error. No
  * escaping is supported (e.g., `\"` is not allowed). */
-int cmdline_read_gramine_args(const char* cmdline, int* out_argc, const char** out_argv) {
-    size_t argv_cnt = 0;
+static int cmdline_read_common(enum cmdline_parse_type type, const char* input, int* out_cnt,
+                               const char** out_array) {
+    /* Choose the appropriate starting and ending string as well as the max token size for
+     * the specified parse type */
+    const char* begin_str;
+    const char* end_str;
+    size_t max_tokens;
+    switch (type) {
+        case CMDLINE_ARGS:
+            begin_str = GRAMINE_ARGS_BEGIN_STR;
+            end_str = GRAMINE_ARGS_END_STR;
+            max_tokens = MAX_ARGV_CNT;
+            break;
+        case CMDLINE_ENVS:
+            begin_str = GRAMINE_ENVS_BEGIN_STR;
+            end_str = GRAMINE_ENVS_END_STR;
+            max_tokens = MAX_ENVS_CNT;
+            break;
+        default:
+            return -PAL_ERROR_INVAL;
+    }
 
-    char* p = strdup(cmdline);
+    size_t curr_cnt = 0;
+
+    char* p = strdup(input);
     if (!p)
         return -PAL_ERROR_NOMEM;
 
-    p = strstr(p, GRAMINE_ARGS_BEGIN_STR);
+    p = strstr(p, begin_str);
     if (!p)
         return -PAL_ERROR_INVAL;
 
-    char* p_end = strstr(p, GRAMINE_ARGS_END_STR);
+    char* p_end = strstr(p, end_str);
     if (!p_end)
         return -PAL_ERROR_INVAL;
 
-    *p_end = '\0'; /* do not count `-gramine-end` and everything after it as arguments */
+    /* do not count the end_str (e.g. `-gramine-args-end`) and everything after it as arguments */
+    *p_end = '\0';
 
-    p += strlen(GRAMINE_ARGS_BEGIN_STR);
+    p += strlen(begin_str);
     while (p) {
         while (*p == ' ' || *p == '\t')
             p++;
         if (*p == '\0')
             break;
 
-        if (argv_cnt == MAX_ARGV)
+        if (curr_cnt == max_tokens)
             return -PAL_ERROR_NOMEM;
 
-        bool arg_in_double_quotes = false;
+        bool token_in_double_quotes = false;
         if (*p == '"') {
             p++;
-            arg_in_double_quotes = true;
+            token_in_double_quotes = true;
         }
 
-        out_argv[argv_cnt] = p;
-        argv_cnt++;
+        out_array[curr_cnt] = p;
+        curr_cnt++;
 
-        if (arg_in_double_quotes) {
+        if (token_in_double_quotes) {
             while (*p != '\0' && *p != '"')
                 p++;
             if (*p == '\0')
@@ -74,8 +97,18 @@ int cmdline_read_gramine_args(const char* cmdline, int* out_argc, const char** o
         }
     }
 
-    *out_argc = argv_cnt;
+    *out_cnt = curr_cnt;
     return 0;
+}
+
+/* parse the gramine cmdline args passed by the VMM */
+int cmdline_read_gramine_args(const char* cmdline, int* out_argc, const char** out_argv) {
+    return cmdline_read_common(CMDLINE_ARGS, cmdline, out_argc, out_argv);
+}
+
+/* parse the environment variables passed by the VMM */
+int cmdline_read_gramine_envs(const char* envs, int* out_envp_cnt, const char** out_envp) {
+    return cmdline_read_common(CMDLINE_ENVS, envs, out_envp_cnt, out_envp);
 }
 
 static uint16_t find_fw_cfg_selector(const char* fw_cfg_name) {
@@ -111,22 +144,41 @@ static uint16_t find_fw_cfg_selector(const char* fw_cfg_name) {
     return __builtin_bswap16(fw_cfg_selector);
 }
 
-int cmdline_init(char* cmdline, size_t cmdline_size) {
-    memset(cmdline, 0, cmdline_size);
+int cmdline_init_args(char* cmdline_args, size_t cmdline_args_size) {
+    memset(cmdline_args, 0, cmdline_args_size);
 
-    uint16_t fw_cfg_selector = find_fw_cfg_selector("opt/gramine/cmdline");
+    uint16_t fw_cfg_selector = find_fw_cfg_selector("opt/gramine/args");
     if (!fw_cfg_selector)
         return -PAL_ERROR_INVAL;
 
     vm_portio_writew(FW_CFG_PORT_SEL, fw_cfg_selector);
-    for (size_t i = 0; i < cmdline_size - 1; i++)
-        cmdline[i] = vm_portio_readb(FW_CFG_PORT_SEL + 1);
+    for (size_t i = 0; i < cmdline_args_size - 1; i++)
+        cmdline_args[i] = vm_portio_readb(FW_CFG_PORT_SEL + 1);
 
-    uint32_t cmdline_len = strlen(cmdline);
-    if (cmdline_len == 0 || cmdline_len >= PATH_MAX)
+    uint32_t cmdline_args_len = strlen(cmdline_args);
+    if (cmdline_args_len == 0 || cmdline_args_len >= PATH_MAX)
         return -PAL_ERROR_INVAL;
 
     /* note that cmdline is guaranteed to be NULL terminated and have at least one symbol */
+    return 0;
+}
+
+int cmdline_init_envs(char* cmdline_envs, size_t cmdline_envs_size) {
+    memset(cmdline_envs, 0, cmdline_envs_size);
+
+    uint16_t fw_cfg_selector = find_fw_cfg_selector("opt/gramine/envs");
+    if (!fw_cfg_selector)
+        return -PAL_ERROR_INVAL;
+
+    vm_portio_writew(FW_CFG_PORT_SEL, fw_cfg_selector);
+    for (size_t i = 0; i < cmdline_envs_size - 1; i++)
+        cmdline_envs[i] = vm_portio_readb(FW_CFG_PORT_SEL + 1);
+
+    uint32_t cmdline_envs_len = strlen(cmdline_envs);
+    if (cmdline_envs_len == 0 || cmdline_envs_len >= MAX_ENVS_SIZE)
+        return -PAL_ERROR_INVAL;
+
+    /* note that envs is guaranteed to be NULL terminated and have at least one symbol */
     return 0;
 }
 
